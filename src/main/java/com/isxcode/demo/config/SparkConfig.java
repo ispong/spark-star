15fbd939b874bb9fe74b7a597766467d2342d164
ispong <ispong@outlook.com>
2021-10-19 16:54:57 +0800

    :sparkles: add spark demo

--- src/main/java/com/isxcode/demo/config/SparkConfig.java
+++ src/main/java/com/isxcode/demo/config/SparkConfig.java
@@ -12,8 +12,6 @@ public class SparkConfig {
 
         // hadoop fs -mkdir /spark-jars
         // hadoop fs -put /data/cdh/cloudera/parcels/CDH/lib/spark/spark-jars/* hdfs://172.23.39.206:30116/spark-jars/
-        // cdh search spark-defaults.conf
-        // spark.yarn.jars=hdfs://172.23.39.206:30116/spark-jars/*.jar
         return SparkSession
                 .builder()
                 .appName("ispong spark demo")
@@ -21,42 +19,9 @@ public class SparkConfig {
                 .master("yarn")
                 .config("spark.yarn.jars", "hdfs://172.23.39.206:30116/spark-jars/*.jar")
                 .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
+                .config("spark.yarn.stagingDir", "/home/dehoop/sparkStaging")
                 .enableHiveSupport()
                 .getOrCreate();
-
-//        return SparkSession
-//                .builder()
-//                .appName("ispong demo")
-//                .master("yarn")
-//                .config("spark.master", "yarn")
-//                .config("spark.yarn.jars", "hdfs://172.23.39.206:30116/spark-jars/**")
-//                .config("spark.yarn.jars", "/home/dehoop/jars/*")
-//                .config("spark.yarn.jars", "hdfs://172.23.39.206:30116/spark-jars/*.jar")
-//                .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
-//                .config("spark.sql.hive.metastore.version", "2.1.1")
-//                .config("spark.sql.hive.metastore.jars", "/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/lib/hive/lib/*")
-//                .config("spark.master", "spark://172.23.39.206:7077")
-//                .config("spark.deploy-mode", "client")
-//                .config("spark.ui.port", "30128") // 没用
-//                .config("spark.yarn.jars", "/home/dehoop/jars/*") // 必须
-//                .config("spark.hadoop.fs.defaultFS", "hdfs://172.23.39.206:30116")
-//                .config("spark.hadoop.yarn.resourcemanager.address", "172.23.39.206:8032")
-//                .config("spark.hadoop.yarn.nodemanager.address", "172.23.39.206:8041")
-//                .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
-//                .config("spark.sql.hive.metastore.version", "2.1.1") // 必须
-//                .config("spark.driver.allowMultipleContexts", "true")
-//                .config("spark.shuffle.service.enabled", "false")
-//                .config("spark.dynamicAllocation.enabled", "false")
-//                .config("spark.executor.memory", "512m")
-//                .config("spark.executor.instances", "1")
-//                .config("spark.executor.cores", "2")
-//                .config("spark.driver.memory", "2g")
-//                .config("spark.sql.broadcastTimeout", "36000")
-//                .config("spark.sql.autoBroadcastJoinThreshold", "-1")
-//                .config("spark.driver.cores", "1")
-//                .config("spark.sql.hive.metastore.jars", "/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/lib/hive/lib/*") // 必须
-//                .enableHiveSupport() // 必须
-//                .getOrCreate();
 
     }
 }
