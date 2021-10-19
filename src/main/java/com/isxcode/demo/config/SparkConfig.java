67c80f1185f8db4ca9f2559373045c2c84c4b908
ispong <ispong@outlook.com>
2021-10-19 12:29:23 +0800

    :sparkles: add spark demo

--- src/main/java/com/isxcode/demo/config/SparkConfig.java
+++ src/main/java/com/isxcode/demo/config/SparkConfig.java
@@ -16,9 +16,9 @@ public class SparkConfig {
                 .master("yarn")
 //                .config("spark.yarn.jars", "/data/cdh/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/jars/*")
 //                .config("spark.sql.hive.metastore.version", "2.1.1")
-                .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
+//                .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
 //                .config("spark.sql.hive.metastore.jars", "/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/lib/hive/lib/*")
-                .enableHiveSupport()
+//                .enableHiveSupport()
                 .getOrCreate();
 
 //        return SparkSession
