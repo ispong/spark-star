63194ea5095134e8992c55699c116d997f7ca3b6
ispong <ispong@outlook.com>
2021-11-03 18:56:40 +0800

    :sparkles: up demo1

--- demo1/src/main/java/com/isxcode/demo1/config/SparkConfig.java
+++ demo1/src/main/java/com/isxcode/demo1/config/SparkConfig.java
@@ -10,11 +10,14 @@ public class SparkConfig {
     @Bean("SparkSession")
     public SparkSession sparkBean() {
 
+        // hadoop fs -mkdir /spark-jars
+        // hadoop fs -put /opt/spark/jars/* hdfs://ispongcluster/spark-jars/
         return SparkSession
                 .builder()
                 .appName("isxcode spark demo1")
                 .master("yarn")
                 .config("hive.metastore.uris", "thrift://172.26.34.162:9083")
+                .config("spark.yarn.jars", "hdfs://ispongcluster/spark-jars/*.jar")
                 .config("spark.hadoop.yarn.resourcemanager.address", "172.26.34.162:8032")
                 .enableHiveSupport()
                 .getOrCreate();
