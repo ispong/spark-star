dbbd992c902a989a696562fa8fe320da84ea0437
ispong <ispong@outlook.com>
2021-11-04 09:50:56 +0800

    :sparkles: up demo1

--- demo1/src/main/java/com/isxcode/demo1/config/SparkConfig.java
+++ demo1/src/main/java/com/isxcode/demo1/config/SparkConfig.java
@@ -10,15 +10,21 @@ public class SparkConfig {
     @Bean("SparkSession")
     public SparkSession sparkBean() {
 
+        String master = "172.26.34.162";
+        String slave1 = "172.26.34.161";
+        String slave2 = "172.16.215.85";
+        String slave3 = "172.16.215.84";
+        String slave4 = "172.16.215.83";
+
         // hadoop fs -mkdir /spark-jars
         // hadoop fs -put /opt/spark/jars/* hdfs://ispongcluster/spark-jars/
         return SparkSession
                 .builder()
                 .appName("isxcode spark demo1")
                 .master("yarn")
-                .config("hive.metastore.uris", "thrift://172.26.34.162:9083")
-                .config("spark.yarn.jars", "hdfs://172.26.34.162:8020/spark-jars/*.jar")
-                .config("spark.hadoop.yarn.resourcemanager.address", "172.26.34.162:8032")
+                .config("hive.metastore.uris", "thrift://master:9083")
+//                .config("spark.yarn.jars", "hdfs://172.26.34.162:8020/spark-jars/*.jar")
+                .config("spark.hadoop.yarn.resourcemanager.address", slave1 + ":8032")
                 .enableHiveSupport()
                 .getOrCreate();
     }
