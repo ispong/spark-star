0d7560eb4f74671d33cf08cc2706e0b404756226
ispong <ispong@outlook.com>
2021-11-04 14:03:04 +0800

    :sparkles: up demo1

--- demo1/src/main/java/com/isxcode/demo1/config/SparkConfig.java
+++ demo1/src/main/java/com/isxcode/demo1/config/SparkConfig.java
@@ -16,10 +16,6 @@ public class SparkConfig {
         String slave3 = "172.16.215.84";
         String slave4 = "172.16.215.83";
 
-        // 配置spark.yarn.jars
-        // hadoop fs -mkdir /spark-jars
-        // hadoop fs -put /opt/spark/jars/* hdfs://ispongcluster/spark-jars/
-
         // 配置spark.yarn.archive
         // cd /opt/spark/jars/
         // zip -q -r spark_jars.zip *
@@ -30,9 +26,8 @@ public class SparkConfig {
                 .appName("isxcode spark demo1")
                 .master("yarn")
                 .config("hive.metastore.uris", "thrift://master:9083")
-//                .config("spark.yarn.jars", "hdfs://" + slave1 + ":8020/spark-jars/*.jar")
                 .config("spark.yarn.preserve.staging.files", true)
-                .config("spark.yarn.archive", "hdfs://" + slave1 + ":8020/spark-yarn/zip/spark_jars.zip")
+                .config("spark.yarn.archive", "hdfs://" + slave2 + ":8020/spark-yarn/zip/spark_jars.zip")
                 .config("spark.hadoop.yarn.resourcemanager.address", slave1 + ":8032")
                 .config("spark.hadoop.yarn.resourcemanager.scheduler.address", slave1 + ":8030")
                 .enableHiveSupport()
