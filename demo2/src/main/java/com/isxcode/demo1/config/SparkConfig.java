f6c3da23c83cbb0259b8d716e8a72713f37186cd
ispong <ispong@outlook.com>
2021-11-05 10:41:39 +0800

    :sparkles: up demo2

--- demo2/src/main/java/com/isxcode/demo1/config/SparkConfig.java
+++ demo2/src/main/java/com/isxcode/demo1/config/SparkConfig.java
@@ -11,16 +11,16 @@ public class SparkConfig {
     public SparkSession sparkBean() {
 
         // 配置spark.yarn.archive
-        // cd /opt/spark/jars/
+        // cd /data/cdh/cloudera/parcels/CDH/lib/spark/jars
         // zip -q -r spark_jars.zip *
         // hadoop fs -mkdir -p /spark-yarn/zip
         // hadoop fs -put spark_jars.zip /spark-yarn/zip/
         return SparkSession
                 .builder()
                 .appName("ispong spark demo")
-                .master("local")
+//                .master("local")
                 .master("yarn")
-                .config("spark.yarn.archive", "")
+                .config("spark.yarn.archive", "hdfs://172.23.39.206:30116/spark-yarn/zip/")
                 .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
                 .enableHiveSupport()
                 .getOrCreate();
