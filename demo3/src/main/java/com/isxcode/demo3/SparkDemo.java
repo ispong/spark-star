cf7160d3cfdad5c6bd570e3f4b6f924a1fb337ba
ispong <ispong@outlook.com>
2021-11-05 14:43:27 +0800

    :sparkles: up demo3

--- demo3/src/main/java/com/isxcode/demo3/SparkDemo.java
+++ demo3/src/main/java/com/isxcode/demo3/SparkDemo.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,14 +14,20 @@ public class SparkDemo {
         String appName = "ispong-demo";
         String master = "local";
 
-        SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
+        SparkConf conf = new SparkConf()
+                .setAppName(appName)
+                .setMaster(master);
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // 获取数据
 
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
         JavaRDD<Integer> distData = sc.parallelize(data);
 
-        System.out.println(distData.toString());
-        System.out.println("hello spark");
+        List<Integer> result = new ArrayList<>();
+        distData.foreach(integer -> result.add(integer + 1));
+        System.out.println("=====> result");
+        result.forEach(System.out::println);
     }
 
 }
