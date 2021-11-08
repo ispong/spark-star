e55fad02cbdb4655af1f339c7838198f8bb9c963
ispong <ispong@outlook.com>
2021-11-08 11:40:28 +0800

    :sparkles: up demo2

--- demo2/src/main/java/com/isxcode/demo1/DemoApplication.java
+++ demo2/src/main/java/com/isxcode/demo1/DemoApplication.java
@@ -1,24 +1,13 @@
 package com.isxcode.demo1;
 
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.stream.Stream;
 
 @RequestMapping
 @RestController
