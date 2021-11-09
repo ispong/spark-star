f477cefcd57b10348ed08b15e6093d257baef4e3
ispong <ispong@outlook.com>
2021-11-09 10:40:29 +0800

    :sparkles: up demo2

--- demo2/src/main/java/com/isxcode/demo1/DemoApplication.java
+++ demo2/src/main/java/com/isxcode/demo1/DemoApplication.java
@@ -1,10 +1,6 @@
 package com.isxcode.demo1;
 
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FilterFunction;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
@@ -20,49 +16,38 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DemoApplication {
 
-	private final SparkSession sparkSession;
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	public DemoApplication(SparkSession sparkSession) {
-		this.sparkSession = sparkSession;
-	}
+    @GetMapping("/demo")
+    public String demo() {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+        // 设置master类型
+        String master = "yarn";
+        // 创建sparkSession
+        SparkSession sparkSession = SparkSession
+                .builder()
+                .appName("ispong-hive-demo")
+                .master(master)
+                .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
+                .config("spark.sql.hive.metastore.version", "2.1.1")
+                .config("spark.sql.hive.metastore.jars", "/data/cdh/cloudera/parcels/CDH/lib/hive/lib/*")
+                .enableHiveSupport()
+                .getOrCreate();
+        // 读取hive中的数据
+        Dataset<Row> rowDataset = sparkSession.sql("select * from rd_dev.houseinfo");
+        // 拦截过滤数据
+        Dataset<Row> result = rowDataset.filter((FilterFunction<Row>) e -> "180".equals(String.valueOf(e.get(0))));
+        // 将结果写入hive中一张新的表
+        result.select("id", "theme")
+                .write()
+                .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
+                .format("Hive")
+                .mode(SaveMode.Overwrite)
+                .saveAsTable("rd_dev.houseinfo_result");
 
-	@GetMapping("/demo")
-	public String demo() {
-
-		// 设置master类型
-		String master = "yarn";
-		// 创建sparkSession
-		SparkSession sparkSession = SparkSession
-				.builder()
-				.appName("ispong-hive-demo")
-				.master(master)
-				.config("hive.metastore.uris", "thrift://172.23.39.206:30123")
-				.config("spark.sql.hive.metastore.version", "2.1.1")
-				.config("spark.sql.hive.metastore.jars", "/data/cdh/cloudera/parcels/CDH/lib/hive/lib/*")
-				.enableHiveSupport()
-				.getOrCreate();
-		// 读取hive中的数据
-		Dataset<Row> rowDataset = sparkSession.sql("select * from rd_dev.houseinfo");
-		// 转为JavaSparkContext
-//		JavaSparkContext sc = JavaSparkContext.fromSparkContext(sparkSession.sparkContext());
-		// 状态数据准备处理
-//		JavaRDD<Row> distData = sc.parallelize(rowDataset.collectAsList());
-		// 开始
-		Dataset<Row> result = rowDataset.filter((FilterFunction<Row>)  e -> "180".equals(String.valueOf(e.get(0))));
-		//数据库内容
-//		Dataset<Row> dataFrame = sparkSession.createDataFrame(result, Object.class);
-		result.select("id", "theme")
-				.write()
-				.option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
-				.format("Hive")
-				.mode(SaveMode.Overwrite)
-				.saveAsTable("rd_dev.houseinfo_result");
-
-		return "hello";
-	}
+        return "hello";
+    }
 
 }
