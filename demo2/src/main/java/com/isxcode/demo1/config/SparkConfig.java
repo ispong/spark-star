8719f7614fc8ad137c970fbe59e6e29f2592ee32
ispong <ispong@outlook.com>
2021-11-16 20:08:01 +0800

    :sparkles: up demo2

--- demo2/src/main/java/com/isxcode/demo1/config/SparkConfig.java
+++ demo2/src/main/java/com/isxcode/demo1/config/SparkConfig.java
@@ -7,23 +7,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SparkConfig {
 
-    @Bean("SparkSession")
-    public SparkSession sparkBean() {
-
-        // 配置spark.yarn.archive
-        // cd /data/cdh/cloudera/parcels/CDH/lib/spark/jars
-        // zip -q -r spark_jars.zip *
-        // hadoop fs -mkdir -p /spark-yarn/zip
-        // hadoop fs -put spark_jars.zip /spark-yarn/zip/
-        return SparkSession
-                .builder()
-                .appName("ispong spark demo")
-                .master("local")
-//                .master("yarn")
-//                .config("spark.yarn.archive", "hdfs://172.23.39.206:30116/spark-yarn/zip/spark_jars.zip")
-                .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
-                .enableHiveSupport()
-                .getOrCreate();
-    }
+//    @Bean("SparkSession")
+//    public SparkSession sparkBean() {
+//
+//        // 配置spark.yarn.archive
+//        // cd /data/cdh/cloudera/parcels/CDH/lib/spark/jars
+//        // zip -q -r spark_jars.zip *
+//        // hadoop fs -mkdir -p /spark-yarn/zip
+//        // hadoop fs -put spark_jars.zip /spark-yarn/zip/
+//        return SparkSession
+//                .builder()
+//                .appName("ispong spark demo")
+//                .master("local")
+////                .master("yarn")
+////                .config("spark.yarn.archive", "hdfs://172.23.39.206:30116/spark-yarn/zip/spark_jars.zip")
+//                .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
+//                .enableHiveSupport()
+//                .getOrCreate();
+//    }
 }
 
