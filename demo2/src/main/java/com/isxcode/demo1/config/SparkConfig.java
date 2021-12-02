bb9a8e96d4706727bdb84a1a06d3d99aa4347cf8
ispong <ispong@outlook.com>
2021-12-02 15:43:30 +0800

    :sparkles: 添加star插件模块

--- demo2/src/main/java/com/isxcode/demo1/config/SparkConfig.java
+++ demo2/src/main/java/com/isxcode/demo1/config/SparkConfig.java
@@ -22,7 +22,7 @@ public class SparkConfig {
 //                .master("yarn")
 //                .config("spark.yarn.archive", "hdfs://172.23.39.206:30116/spark-yarn/zip/spark_jars.zip")
 //                .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
-                .enableHiveSupport()
+//                .enableHiveSupport()
                 .getOrCreate();
     }
 }
