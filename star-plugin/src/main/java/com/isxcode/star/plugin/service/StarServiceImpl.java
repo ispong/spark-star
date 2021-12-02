5e94f8014f746b1532be66fc3111a7cdd1514b72
ispong <ispong@outlook.com>
2021-12-02 15:52:16 +0800

    :sparkles: 添加star插件模块

--- star-plugin/src/main/java/com/isxcode/star/plugin/service/StarServiceImpl.java
+++ star-plugin/src/main/java/com/isxcode/star/plugin/service/StarServiceImpl.java
@@ -1,19 +1,27 @@
 package com.isxcode.star.plugin.service;
 
+import com.isxcode.star.plugin.properties.StarProperties;
 import org.apache.spark.sql.SparkSession;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StarServiceImpl implements StarService {
+
+    private final StarProperties starProperties;
+
+    public StarServiceImpl(StarProperties starProperties) {
+        this.starProperties = starProperties;
+    }
 
     @Override
     public SparkSession generateNewSession() {
 
         return SparkSession
                 .builder()
-                .appName("ispong-hive-demo")
-                .master("yarn")
-                .config("hive.metastore.uris", "thrift://172.23.39.206:30123")
-                .config("spark.sql.hive.metastore.version", "2.1.1")
-                .config("spark.sql.hive.metastore.jars", "/data/cdh/cloudera/parcels/CDH/lib/hive/lib/*")
+                .master(starProperties.getMaster())
+                .config("hive.metastore.uris", starProperties.getHiveMetastoreUris())
+                .config("spark.sql.hive.metastore.version", starProperties.getHiveMetastoreVersion())
+                .config("spark.sql.hive.metastore.jars", starProperties.getHiveMetastoreJars())
                 .enableHiveSupport()
                 .getOrCreate();
     }
