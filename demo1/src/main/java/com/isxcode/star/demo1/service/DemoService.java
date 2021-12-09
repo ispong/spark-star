20e5c97758dc4b46b47647df0b366a3948cea34e
ispong <ispong@outlook.com>
2021-12-09 11:12:38 +0800

    :sparkles: demo1 测试本地yarn

--- demo1/src/main/java/com/isxcode/star/demo1/service/DemoService.java
+++ demo1/src/main/java/com/isxcode/star/demo1/service/DemoService.java
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class DemoService {
 
-    private final SparkSession sparkSession;
-
-    public DemoService(SparkSession sparkSession) {
-        this.sparkSession = sparkSession;
-    }
+//    private final SparkSession sparkSession;
+//
+//    public DemoService(SparkSession sparkSession) {
+//        this.sparkSession = sparkSession;
+//    }
 
     public void executeSql(String sql) {
 
-        Dataset<Row> dataset = sparkSession.sql(sql);
-        dataset.show();
+//        Dataset<Row> dataset = sparkSession.sql(sql);
+//        dataset.show();
     }
 }
