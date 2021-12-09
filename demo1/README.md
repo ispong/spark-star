39d4d0177dc68818564772a96bff86fd6059f986
ispong <ispong@outlook.com>
2021-12-09 11:30:21 +0800

    :sparkles: demo1 测试本地yarn

--- demo1/README.md
+++ demo1/README.md
@@ -1,3 +1,16 @@
+### 前提
+
+```bash
+sudo vim /etc/profile
+
+# === sudo vim /etc/profile ===
+export HADOOP_HOME=/data/cdh/cloudera/parcels/CDH/lib/hadoop
+export HADOOP_YARN_HOME=/data/cdh/cloudera/parcels/CDH/lib/hadoop-yarn
+export SPARK_HOME=/data/cdh/cloudera/parcels/CDH/lib/spark
+# === sudo vim /etc/profile ===
+
+source /etc/profile
+```
 
 ```http request
 GET http://localhost:30156/demo
--- demo1/src/main/java/com/isxcode/star/demo1/config/SparkConfig.java
+++ demo1/src/main/java/com/isxcode/star/demo1/config/SparkConfig.java
@@ -7,16 +7,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SparkConfig {
 
-//    @Bean("SparkSession")
-//    public SparkSession sparkBean() {
-//
-//        return SparkSession
-//                .builder()
-//                .appName("spark star demo1")
-//                .master("yarn")
-//                .config("spark.ui.port", "30157")
-//                .config("hive.metastore.uris", "thrift://dcloud-dev:30123")
-//                .enableHiveSupport()
-//                .getOrCreate();
-//    }
+    @Bean("SparkSession")
+    public SparkSession sparkBean() {
+
+        return SparkSession
+                .builder()
+                .appName("spark star demo1")
+                .master("yarn")
+                .config("spark.ui.port", "30157")
+                .config("hive.metastore.uris", "thrift://dcloud-dev:30123")
+                .enableHiveSupport()
+                .getOrCreate();
+    }
 }
