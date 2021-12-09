52d3ef9acdf5b2bf009f3b5064c459e01fec62be
ispong <ispong@outlook.com>
2021-12-09 12:10:46 +0800

    :sparkles: demo1 测试本地yarn

--- demo1/src/main/java/com/isxcode/star/demo1/config/SparkConfig.java
+++ demo1/src/main/java/com/isxcode/star/demo1/config/SparkConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SparkConfig {
 
-
     @ConditionalOnClass(WebConfig.class)
     @Bean("SparkSession")
     public SparkSession sparkBean() {
