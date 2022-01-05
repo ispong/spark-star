e8d8daf1d15bd15f360f77765f0fd3d32d6e53cd
ispong <ispong@outlook.com>
2022-01-05 15:58:40 +0800

    :sparkles: 测试demo1 动态写入配置文件

--- demos/demo1/src/main/java/com/isxcode/star/demo1/config/SparkConfig.java
+++ demos/demo1/src/main/java/com/isxcode/star/demo1/config/SparkConfig.java
@@ -12,7 +12,6 @@ import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
