dcaeac27d5c92bb656a8def3b6f37125bdc43315
ispong <ispong@outlook.com>
2022-01-06 18:00:06 +0800

    :sparkles: 整和star-plugin模块

--- star-plugin/bin/deploy.sh
+++ star-plugin/bin/deploy.sh
@@ -12,7 +12,7 @@ POM_PATH=../pom.xml
 mvn clean package -f "${POM_PATH}"
 
 # 关闭原有进程
-for metaResult in $(ps -e -o pid,command | grep demo1-0.0.1.jar)
+for metaResult in $(ps -e -o pid,command | grep star-plugin.jar)
 do
     if [ "$metaResult" == java ]; then
       STAR_PID=${PRE_VAL}
@@ -36,12 +36,12 @@ fi
 
 # 重新解压把环境变量放到jar中
 mkdir -p ../target/build
-unzip ../target/demo1-0.0.1.jar -d ../target/build/
+unzip ../target/star-plugin.jar -d ../target/build/
 cp /data/cdh/cloudera/parcels/CDH/lib/hadoop/etc/hadoop/*.xml ../target/build/BOOT-INF/classes/
 cd ../target/build/ || exit
-jar -cvfM0 ../demo1-0.0.1.jar ./*
+jar -cvfM0 ../star-plugin.jar ./*
 
 # 启动项目
-STAR_APP=../demo1-0.0.1.jar
+STAR_APP=../star-plugin.jar
 nohup java -jar -Xmx2048m "${STAR_APP}" --spring.profiles.active="${ENV}" >> "${STAR_LOG}" 2>&1 &
 echo "部署成功"
