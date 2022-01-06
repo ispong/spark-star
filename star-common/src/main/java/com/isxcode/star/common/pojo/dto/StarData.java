f98e7f645b87b37b4b1fbed5a0405a03d22a679d
ispong <ispong@outlook.com>
2022-01-06 18:47:13 +0800

    :sparkles: 整和star-plugin模块

--- star-common/src/main/java/com/isxcode/star/common/pojo/dto/StarData.java
+++ star-common/src/main/java/com/isxcode/star/common/pojo/dto/StarData.java
@@ -1,12 +1,16 @@
 package com.isxcode.star.common.pojo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StarData {
 
     private List<String> columnNames;
