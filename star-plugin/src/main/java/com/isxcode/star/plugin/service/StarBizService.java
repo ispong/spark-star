e839ae79bd6923457454e86cbf7a4bd1f9d6f8a5
ispong <ispong@outlook.com>
2022-01-08 15:27:46 +0800

    :bug: 修复spark作业无法异步执行

--- star-plugin/src/main/java/com/isxcode/star/plugin/service/StarBizService.java
+++ star-plugin/src/main/java/com/isxcode/star/plugin/service/StarBizService.java
@@ -5,18 +5,28 @@ import com.isxcode.star.common.pojo.dto.StarData;
 import com.isxcode.star.common.response.StarRequest;
 import com.isxcode.star.plugin.exception.StarException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Service
 public class StarBizService {
 
+    private final SparkSession sparkSession;
+
     private final StarService starService;
 
     private final StarSyncService starSyncService;
 
-    public StarBizService(StarService starService,
+    public StarBizService(SparkSession sparkSession, StarService starService,
                           StarSyncService starSyncService) {
+        this.sparkSession = sparkSession;
 
         this.starService = starService;
         this.starSyncService = starSyncService;
@@ -29,7 +39,23 @@ public class StarBizService {
             throw new StarException(StarExceptionEnum.REQUEST_VALUE_EMPTY);
         }
 
-        return starService.querySql(starRequest.getSql());
+        Dataset<Row> rowDataset = sparkSession.sql(starRequest.getSql());
+
+        String[] columns = rowDataset.columns();
+        StarData.StarDataBuilder starDataBuilder = StarData.builder().columnNames(Arrays.asList(columns));
+
+        List<List<String>> dataList = new ArrayList<>();
+        List<Row> rows = rowDataset.collectAsList();
+        rows.forEach(e -> {
+            List<String> metaData = new ArrayList<>();
+            for (int i = 0; i < e.size(); i++) {
+                metaData.add(String.valueOf(e.get(i)));
+            }
+            dataList.add(metaData);
+        });
+
+        return starDataBuilder.dataList(dataList).build();
+
     }
 
     public StarData executeSyncWork(StarRequest starRequest, String url) {
