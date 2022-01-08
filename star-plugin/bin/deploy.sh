5cf090f643da6d56d9343be9a1f46497132959e2
ispong <ispong@outlook.com>
2022-01-08 12:01:58 +0800

    :sparkles: 添加kafka支持事件推送

--- star-plugin/bin/deploy.sh
+++ star-plugin/bin/deploy.sh
@@ -9,7 +9,7 @@ done
 
 # mvn打包
 POM_PATH=../pom.xml
-mvn clean package -f "${POM_PATH}"
+mvn clean package -f "${POM_PATH}" || exit
 
 # 关闭原有进程
 for metaResult in $(ps -e -o pid,command | grep star-plugin.jar)
