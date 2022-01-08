464edbd6c4721212771a8eb79dfd1bb5eaa7b1ef
ispong <ispong@outlook.com>
2022-01-08 16:19:58 +0800

    :sparkles: 添加kafka监听事件

--- star-common/src/main/java/com/isxcode/star/common/config/KafkaConfig.java
+++ star-common/src/main/java/com/isxcode/star/common/config/KafkaConfig.java
@@ -1,6 +1,6 @@
 package com.isxcode.star.common.config;
 
-import com.isxcode.star.common.properties.StarPluginProperties;
+import com.isxcode.star.common.properties.StarNodeProperties;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
@@ -19,17 +19,18 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConfig {
 
-    private final StarPluginProperties starPluginProperties;
+    private final StarNodeProperties starNodeProperties;
 
-    public KafkaConfig(StarPluginProperties starPluginProperties) {
-        this.starPluginProperties = starPluginProperties;
+    public KafkaConfig(StarNodeProperties starNodeProperties) {
+
+        this.starNodeProperties = starNodeProperties;
     }
 
     @Bean
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, starPluginProperties.getKafkaConfig().get(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG));
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, starPluginProperties.getKafkaConfig().get(ConsumerConfig.GROUP_ID_CONFIG));
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, starNodeProperties.getKafkaConfig().get(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG));
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, starNodeProperties.getKafkaConfig().get(ConsumerConfig.GROUP_ID_CONFIG));
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
