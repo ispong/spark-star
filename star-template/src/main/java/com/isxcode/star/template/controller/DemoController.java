406159897eb0cea7bc3127fabbf266516f9374c5
ispong <ispong@outlook.com>
2022-01-13 17:22:36 +0800

    :sparkles: 添加日志查询接口

--- star-template/src/main/java/com/isxcode/star/template/controller/DemoController.java
+++ star-template/src/main/java/com/isxcode/star/template/controller/DemoController.java
@@ -5,6 +5,7 @@ import com.isxcode.star.common.response.StarResponse;
 import com.isxcode.star.common.template.StarTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +28,28 @@ public class DemoController {
             .build();
 
         StarResponse starResponse = starTemplate.build().executeSql(starRequest);
+        System.out.println(starResponse.toString());
+    }
+
+    @GetMapping("/getLog")
+    public void getLog(@RequestParam String appId) {
+
+        StarRequest starRequest = StarRequest.builder()
+            .appId(appId)
+            .build();
+
+        StarResponse starResponse = starTemplate.build().getLog(starRequest);
+        System.out.println(starResponse.toString());
+    }
+
+    @GetMapping("/stopJob")
+    public void stopJob(@RequestParam String appId) {
+
+        StarRequest starRequest = StarRequest.builder()
+            .appId(appId)
+            .build();
+
+        StarResponse starResponse = starTemplate.build().stopJob(starRequest);
         System.out.println(starResponse.toString());
     }
 }
