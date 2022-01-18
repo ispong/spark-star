4451a18f7e0c7e88bb741a1601e42b5211a19d20
ispong <ispong@outlook.com>
2022-01-18 15:17:41 +0800

    :bug: 修复spark获取外部参数

--- star-common/src/main/java/com/isxcode/star/common/response/StarRequest.java
+++ star-common/src/main/java/com/isxcode/star/common/response/StarRequest.java
@@ -32,4 +32,84 @@ public class StarRequest {
     private Integer limit = 2000;
 
     private String appId;
+
+    public String getExecuteId() {
+        return executeId;
+    }
+
+    public void setExecuteId(String executeId) {
+        this.executeId = executeId;
+    }
+
+    public boolean isHasReturn() {
+        return hasReturn;
+    }
+
+    public void setHasReturn(boolean hasReturn) {
+        this.hasReturn = hasReturn;
+    }
+
+    public String getSql() {
+        return sql;
+    }
+
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+
+    public List<String> getSqls() {
+        return sqls;
+    }
+
+    public void setSqls(List<String> sqls) {
+        this.sqls = sqls;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
 }
