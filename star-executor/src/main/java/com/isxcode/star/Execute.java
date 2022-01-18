1c0ac5c10cc47b4e76a24a10e59d3e212f92136f
ispong <ispong@outlook.com>
2022-01-18 11:38:19 +0800

    :bug: 修复线程异常处理线程

--- star-executor/src/main/java/com/isxcode/star/Execute.java
+++ star-executor/src/main/java/com/isxcode/star/Execute.java
@@ -9,16 +9,16 @@ public class Execute {
     public static void main(String[] args) {
 
         // 获取请求参数
-        StarRequest starRequest = JSON.parseObject(String.valueOf(args[0]), StarRequest.class);
-        System.out.println("显示请求参数：" + starRequest.toString());
+//        StarRequest starRequest = JSON.parseObject(String.valueOf(args[0]), StarRequest.class);
+//        System.out.println("显示请求参数：" + starRequest.toString());
 
         // 构建新的sparkSession
         SparkSession sparkSession = SparkSession.builder().getOrCreate();
 
         // 执行spark
         System.out.println("开始执行");
-        sparkSession.sql("use " + starRequest.getDatabase());
-        sparkSession.sql(starRequest.getSql());
+//        sparkSession.sql("use " + starRequest.getDatabase());
+        sparkSession.sql("insert into rd_dev.ispong_table(username, age, lucky_date) values ('ispong', 18, '2021-12-12 12:12:12')");
 
         // 停止session
         sparkSession.stop();
