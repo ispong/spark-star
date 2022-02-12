b1071cd62b2f2144c061118305ca276d74528422
ispong <ispong@outlook.com>
2022-02-12 10:31:50 +0800

    :sparkles: 测试demo2

--- demos/demo2/src/main/java/com/isxcode/star/demo2/Demo.java
+++ demos/demo2/src/main/java/com/isxcode/star/demo2/Demo.java
@@ -18,7 +18,7 @@ public class Demo {
             .config("spark.driver.memory", "5g")
             //.config("spark.num.executors", 4)  // 一个作业设置多少个executor
             // 1g 500m Size must be specified as bytes (b), kibibytes (k), mebibytes (m), gibibytes (g), tebibytes (t), or pebibytes(p). E.g. 50b, 100k, or 250m.
-            .config("spark.executor.memory", "8g") //   executor一共可申请内存,受限于yarn的总内存 (yarn.nodemanager.resource.memory-mb)
+            .config("spark.executor.memory", "7g") //   executor一共可申请内存,受限于yarn的总内存 (yarn.nodemanager.resource.memory-mb)
             .config("spark.executor.cores", 4) // executor一共可申请的内核,受限于yarn的vcores (yarn.nodemanager.resource.cpu-vcores)
             .config("spark.yarn.historyServer.allowTracking", true) // 允许historyServer监控
             .config("spark.sql.storeAssignmentPolicy", "LEGACY")
