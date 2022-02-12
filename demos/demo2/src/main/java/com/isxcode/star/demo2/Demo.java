10e641c4b14555a9b8c78796f570756736dbc6f9
ispong <ispong@outlook.com>
2022-02-12 11:11:32 +0800

    :sparkles: 测试demo2

--- demos/demo2/src/main/java/com/isxcode/star/demo2/Demo.java
+++ demos/demo2/src/main/java/com/isxcode/star/demo2/Demo.java
@@ -15,13 +15,17 @@ public class Demo {
             .config("deploy-mode", "cluster")
             .config("spark.yarn.queue", "default")
             .config("hive.metastore.uris", "thrift://localhost:9083")
-            .config("spark.driver.memory", "2g") // 不用给太多
+            .config("spark.driver.memory", "2g") // driver进程的内存 不用给太多
+            .config("spark.driver.cores", 1) // driver进程的CPU core 数量
             // 1g 500m Size must be specified as bytes (b), kibibytes (k), mebibytes (m), gibibytes (g), tebibytes (t), or pebibytes(p). E.g. 50b, 100k, or 250m.
             // executor一共可申请内存,受限于yarn的总内存 (yarn.nodemanager.resource.memory-mb) 不可占满yarn的内存
             .config("spark.executor.memory", "1g")
             // executor一共可申请的内核,受限于yarn的vcores (yarn.nodemanager.resource.cpu-vcores)
             .config("spark.executor.core", 4)
+            // 经过产看官方文档发现 --num-executors该属性只有在YARN模式下有效
             .config("spark.num.executors", 10)  // 一个作业设置多少个executor
+            .config("spark.dynamicAllocation.enabled", true)
+            .config("spark.shuffle.service.enabled", true)
             .config("spark.yarn.historyServer.allowTracking", true) // 允许historyServer监控
             .config("spark.sql.storeAssignmentPolicy", "LEGACY")
             .enableHiveSupport()
