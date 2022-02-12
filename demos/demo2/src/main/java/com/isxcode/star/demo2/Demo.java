7940e5d08c56e305e75e637b7337603547d3a34e
ispong <ispong@outlook.com>
2022-02-12 12:27:54 +0800

    :sparkles: 测试demo2

--- demos/demo2/src/main/java/com/isxcode/star/demo2/Demo.java
+++ demos/demo2/src/main/java/com/isxcode/star/demo2/Demo.java
@@ -19,18 +19,17 @@ public class Demo {
             .config("deploy-mode", "cluster")
             .config("spark.yarn.queue", "default")
             .config("hive.metastore.uris", "thrift://localhost:9083")
-            .config("spark.driver.memory", "2g") // driver进程的内存，除非数据量特别大，否则不用给太多
-            .config("spark.driver.cores", 1) // driver进程的cpu-vcore个数，除非数据量特别大，否则不用给太多
-            // 单个executor最大可申请内存,受限于yarn的总内存 (yarn-site.xml文件中的yarn.nodemanager.resource.memory-mb配置，需要重启yarn) 不可占满yarn的内存
-            // 实际申请不会打满，一般设置2g，会申请1.5个g
+            .config("spark.driver.memory", "2g") // 设置driver进程的内存，除非数据量特别大，否则不用给太多
+            .config("spark.driver.cores", 1) // 设置driver进程的cpu-core个数，除非数据量特别大，否则不用给太多
+            // 单个executor最大可申请使用内存,受限于yarn的总内存 (yarn-site.xml文件中的yarn.nodemanager.resource.memory-mb配置，修改后需要重启yarn) 不可占满yarn的总内存
+            // 实际申请不会打满，一般设置2g，会申请1.3个g左右
             .config("spark.executor.memory", "2g")
-            // 单个executor使用cpu-vcore的个数, 受限于yarn的vcores总个数 (yarn-site.xml文件中的yarn.nodemanager.resource.cpu-vcores配置，需要重启yarn) 不可占满yarn的vcore
+            // 单个executor使用cpu-core的个数, 受限于yarn的cores总个数 (yarn-site.xml文件中的yarn.nodemanager.resource.cpu-vcores配置，修改后需要重启yarn) 不可占满yarn的vcore总个数
             // 一个core处理，可处理100M+的数据量，设置太多会浪费
             .config("spark.executor.cores", 1)
-            // 一个作业需要使用executor个数，看情况设置，设置太多的话，会浪费资源，设置太少的话，运行会慢。
-            // 如果资源不足的话，设置4，可现资源只够3个使用，它会等待一会儿资源释放，这边会变慢，如果资源还是不足的话，它会申请2个，然后继续执行
-            // 如果资源一个都不够的话，spark作业直接卡死
-            // 设置太多也会浪费
+            // 每个作业需要使用executor的个数，看情况设置，设置太多的话，会浪费资源，设置太少的话，运行会慢。设置太多申请资源，也会变慢，且浪费资源。
+            // 如果资源不足的话，设置为4，可现有资源只够3个使用，它会等待资源释放，时间会变慢，如果资源还是不足，它会申请2个，然后执行spark作业。
+            // 如果等待后资源一个都不够的话，spark作业直接卡死失败。
             .config("spark.executor.instances", 1)
 //            高级玩法-动态分配，暂时不需要，上面的配置调一调，应该就快起来了
 //            .config("spark.dynamicAllocation.enabled", true)
