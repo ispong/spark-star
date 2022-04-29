3834990582a99f32dbb4f977e6217656ee7be5c3
ispong <ispong@outlook.com>
2022-04-29 19:02:46 +0800

    :sparkles: 使用系统本地版本

--- build.sh
+++ build.sh
@@ -63,6 +63,7 @@ unzip "${BASE_PATH}"/star-plugin/target/star-plugin.jar -d "${TMP_BUILD_DIR}"/
 rm -f "${TMP_BUILD_DIR}"/BOOT-INF/lib/janino-*.jar
 cp "${HIVE_HOME}"/lib/* "${TMP_BUILD_DIR}"/BOOT-INF/lib/
 cp "${SPARK_HOME}"/jars/* "${TMP_BUILD_DIR}"/BOOT-INF/lib/
+rm -f "${TMP_BUILD_DIR}"/BOOT-INF/lib/*.pom
 cp "${HADOOP_HOME}"/etc/hadoop/* "${TMP_BUILD_DIR}"/BOOT-INF/classes/
 cd "${TMP_BUILD_DIR}" && jar -cvfM0 star-plugin.jar ./*
 
